ec0e52026eb9ab3a09f3c12dab68291a551dbdb8
timhdavis <timhdavis@yahoo.com>
2016-10-23 11:37:34 -0700

    Added form for Support Requests (on main page)
    -can check that support requests are added by going to
    http://localhost:9000/supportRequests
    -Also cleaned up some of the forms (need to be improved to actually
    look good)
    -changed “Project Name” to “Minute.tech”

--- target/scala-2.11/twirl/main/views/html/index.template.scala
+++ target/scala-2.11/twirl/main/views/html/index.template.scala
@@ -45,21 +45,41 @@ Seq[Any](format.raw/*1.78*/("""
     </div>
     
     <!--test form for technicians; to be moved; TODO: -->
-  
+  <br>Technicians
   <form action=""""),_display_(/*16.18*/routes/*16.24*/.TechnicianController.addTechnician()),format.raw/*16.61*/("""" method="post">
+    First Name:<br>
       <input type="text" name="firstName" />
+     <br>Last Name:<br>
       <input type="text" name="lastName" />
+      <br>
       <button>Add Technician</button>
   </form>
+  
+  <br>Support Requests
+  
+  <form action=""""),_display_(/*27.18*/routes/*27.24*/.SupportRequestController.addSupportRequest()),format.raw/*27.69*/("""" method="post">
+    Title:<br>
+      <input type="text" name="title" />
+      <br>Body:<br>
+      <input type="text" name="body" />
+      <br>Difficulty:<br>
+      <input type="number" name="difficulty" />
+      <br>
+      <button>Add Support Request</button>
+  </form>
+  
+  <br>
+  
+  <!-- end ADDED. -->
 
   <div class="col-lg-6">
     <div class="well">
-      """),_display_(/*24.8*/login(loginForm)),format.raw/*24.24*/("""
-    """),format.raw/*25.5*/("""</div>
+      """),_display_(/*44.8*/login(loginForm)),format.raw/*44.24*/("""
+    """),format.raw/*45.5*/("""</div>
 
     <div class="well">
-      """),_display_(/*28.8*/views/*28.13*/.html.account.signup.createFormOnly(signupForm)),format.raw/*28.60*/("""
-    """),format.raw/*29.5*/("""</div>
+      """),_display_(/*48.8*/views/*48.13*/.html.account.signup.createFormOnly(signupForm)),format.raw/*48.60*/("""
+    """),format.raw/*49.5*/("""</div>
   </div>
   
   
@@ -83,11 +103,11 @@ Seq[Any](format.raw/*1.78*/("""
 object index extends index_Scope0.index
               /*
                   -- GENERATED --
-                  DATE: Sun Oct 23 10:48:37 PDT 2016
+                  DATE: Sun Oct 23 11:33:14 PDT 2016
                   SOURCE: /Users/timhdavis/GitHub/Play! framework/PlayStartApp/app/views/index.scala.html
-                  HASH: 0466dba60e06d751b9648c0b7e91517e06131eaa
-                  MATRIX: 789->1|943->80|957->87|1037->91|1068->96|1108->110|1122->116|1179->153|1254->77|1282->189|1310->192|1337->211|1376->213|1406->218|1434->226|1464->229|1631->369|1646->375|1704->412|1940->622|1977->638|2009->643|2073->681|2087->686|2155->733|2187->738
-                  LINES: 27->1|31->3|31->3|33->3|34->4|34->4|34->4|34->4|36->1|38->5|40->7|40->7|40->7|42->9|42->9|43->10|49->16|49->16|49->16|57->24|57->24|58->25|61->28|61->28|61->28|62->29
+                  HASH: 338b6ab9bc577f545a09cf5e5ba3af8655849be0
+                  MATRIX: 789->1|943->80|957->87|1037->91|1068->96|1108->110|1122->116|1179->153|1254->77|1282->189|1310->192|1337->211|1376->213|1406->218|1434->226|1464->229|1646->384|1661->390|1719->427|2001->682|2016->688|2082->733|2470->1095|2507->1111|2539->1116|2603->1154|2617->1159|2685->1206|2717->1211
+                  LINES: 27->1|31->3|31->3|33->3|34->4|34->4|34->4|34->4|36->1|38->5|40->7|40->7|40->7|42->9|42->9|43->10|49->16|49->16|49->16|60->27|60->27|60->27|77->44|77->44|78->45|81->48|81->48|81->48|82->49
                   -- GENERATED --
               */
           
